505846626fe055874827dc70ae59ed157886b467
Liam Maguire <18344533@students.latrobe.com>
2021-12-30 12:24:56 +1100

    Simplify view Animal

--- src/app/view-animal/view-animal.component.ts
+++ src/app/view-animal/view-animal.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { getStorage, ref, getDownloadURL } from "firebase/storage";
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
-import { doc, getDoc, getFirestore } from "firebase/firestore";
 
 @Component({
   selector: 'app-view-animal',
@@ -10,33 +8,19 @@ import { doc, getDoc, getFirestore } from "firebase/firestore";
 })
 export class ViewAnimalComponent implements OnInit {
   @Input() model: any;   // instead of any, specify your type
-  firestore = getFirestore();
-  constructor(private route: ActivatedRoute) {
+  constructor() {
   }
 
   storage = getStorage();
-  imgURL = "" // promise of string 
-  center = { lat: -30, lng: 133.3 }; // move this to not be hardcoded
-  zoom = 9
-  markerPosition ={ draggable: false, position: this.center };
-  options: google.maps.MapOptions = {
-    streetViewControl: false
-  };
-
+  imgURL = "" // promise of string
   ngOnInit(): void {
-    if (this.model == undefined || this.model.id == "") {
-      const routeParams = this.route.snapshot.paramMap;
-      const idFromRoute = String(routeParams.get('id'));
-      this.getAnimal(idFromRoute);
-    } else {
+    if (this.model !== undefined || this.model.id !== "") {
       this.getURL();
-    }
-    console.log(this.model)
-    //this.getURL();
+    } 
   }
 
   // not a great design but just want it to work for now
-  getURL() {
+  async getURL() {
     const pathReference = ref(this.storage, `${this.model?.id}.png`);
     return getDownloadURL(pathReference)
       .then((url) => {
@@ -44,19 +28,5 @@ export class ViewAnimalComponent implements OnInit {
         this.imgURL = url;
         return url
       });
-  }
-
-  async getAnimal(id: string) {
-    const docRef = doc(this.firestore, "cities", id);
-    const docSnap = await getDoc(docRef);
-    if (docSnap.exists()) {
-      console.log("Document data:", docSnap.data());
-      let obj = docSnap.data();
-      obj.id = docSnap.id;
-      this.model = obj;
-      this.getURL()
-    } else {
-      // err handle + redirect
-    }
   }
 }
